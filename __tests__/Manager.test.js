1e614d7e7f25ba5a2b0f7b83d23778e0117b0188
uiido <uiido@yahoo.com>
2023-01-03 22:24:24 -0500

    Filled in html for manager card generation - will test before filling in other cards

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,21 +1,5 @@
-// Generate Manager
 const Manager = require("../lib/Manager");
 
 describe("Manager", () => {
-
-});
-
-// Generate Engineer
-const Engineer = require("../lib/Engineer");
-
-describe("Engineer", () => {
-
-});
-
-
-// Generate Intern
-const Intern = require("../lib/Intern");
-
-describe("Intern", () => {
 
 });
--- src/page-template.js
+++ src/page-template.js
@@ -1,9 +1,32 @@
 // create the team
 const generateTeamMembers = (team) => {
-    return
+
+    // generate manager card
+    const generateManager = (manager) => {
+        return
+        `
+    <div class="card employee-card">
+    <div class="card-header">
+        <h2>${manager.getName()}</h2>
+        <h3>${manager.getRole()}</h3>
+    </div>
+    <div class="card-body">
+        <ul class="card-id-list">
+            <li class="id-number">ID: ${manager.getId()}</li>
+            <li class="id-email">Email: <a href="mailto:${manager.getEmail()}">${manager.getEmail()}</a></li>
+            <li class="id-office">Office number: ${manager.getOfficeNumber()}</li>
+        </ul>
+    </div>
+</div>
+    `};
+
+    // generate engineer card
+    return `
     `
 
-`
+        // generate intern card
+        `
+    `
 };
 
 // export function to generate entire page
