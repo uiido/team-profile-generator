a9844fead7ee6cc357a54a793d2e172d0b0b37fa
uiido <uiido@yahoo.com>
2023-01-03 23:50:46 -0500

    Added intern questions

--- index.js
+++ index.js
@@ -108,3 +108,35 @@ function addEngineer() {
 }
 
 // Intern
+function addIntern() {
+    inquirer.prompt([
+
+        {
+            type: "input",
+            name: "internName",
+            message: "What is this intern's name?"
+        },
+
+        {
+            type: "input",
+            name: "internId",
+            message: "What is this intern's employee ID number?"
+        },
+
+        {
+            type: "input",
+            name: "internEmail",
+            message: "What is this intern's email address?"
+        },
+
+        {
+            type: "input",
+            name: "internSchool",
+            message: "What school does this intern attend?"
+        }
+
+    ]).then(answers => {
+        const intern = new Intern(answers.internName, answers.internId, answers.internEmail, answers.internSchool);
+        teamArray.push(intern);
+    });
+}
