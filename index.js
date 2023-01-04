d55a7207e260be027124ac80cd97a112b34ff716
uiido <uiido@yahoo.com>
2023-01-03 23:24:03 -0500

    Created team questions on index.js

--- index.js
+++ index.js
@@ -7,3 +7,36 @@ const fs = require("fs");
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "your-team.html");
 const generateTeam = require("./src/page-template.js")
+
+
+
+// Questions to build a team and fill in their information
+function newTeam() {
+    inquirer.prompt([{
+        type: "list",
+        message: "What position are you adding?",
+        name: "addEmployeePrompt",
+        choices: ["Manager", "Engineer", "Intern", "My team is complete!"]
+    }]).then(function (userInput) {
+        switch (userInput.addEmployeePrompt) {
+            case "Manager":
+                addManager();
+                break;
+            case "Engineer":
+                addEngineer();
+                break;
+            case "Intern":
+                addIntern();
+                break;
+        }
+
+    })
+}
+
+// Manager
+
+
+// Engineer
+
+
+// Intern
