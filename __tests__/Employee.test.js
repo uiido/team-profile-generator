db460e5fd7524c8cf94ebbdeda36c180f535e633
uiido <uiido@yahoo.com>
2023-01-14 19:16:57 -0500

    Minor tweaks to catch up to speed with video review

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -3,8 +3,18 @@ const Employee = require("../lib/Employee");
 describe("Employee", () => {
     it('should have a name', () => {
         const employee = new Employee('Guy', 1, 'fun@guy.com');
-        expect(employee.id).toEqual(1);
+        expect(employee.name).toEqual('Guy');
     });
-    it('should have an id', () => { })
-    it('should have an email', () => { })
+    it('should have an id', () => {
+        const employee = new Employee('Guy', 1, 'fun@guy.com');
+        expect(employee.id).toEqual(1);
+    })
+    it('should have an email', () => {
+        const employee = new Employee('Guy', 1, 'fun@guy.com');
+        expect(employee.email).toEqual('fun@guy.com');
+    })
+    it('should have a role', () => {
+        const employee = new Employee('Guy', 1, 'fun@guy.com');
+        expect(employee.getRole()).toEqual('Employee');
+    })
 });
